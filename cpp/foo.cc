845f89e7211673dc784ccddab9ae43f4ecf26baf
Salzman Alexis <alexis.salzman@ec-nantes.fr>
2026-07-30 14:33:19 +0200

    change setf/foo

--- cpp/foo.cc
+++ cpp/foo.cc
@@ -3,6 +3,7 @@
 double foo(const std::shared_ptr<dolfinx::fem::Function<double>> field)
 {
    auto r = field->x()->array();
+   auto size_local=field->function_space()->dofmap()->index_map->size_local();
 
-   return std::accumulate(r.begin(), r.end(), 0.);
+   return std::accumulate(r.begin(), r.begin()+size_local, 0.);
 }
--- python/bar/util.py
+++ python/bar/util.py
@@ -2,4 +2,5 @@ from __future__ import annotations
 import numpy as np
 from dolfinx.fem.function import Function
 def setf(field:Function):
-    field.x.array[:]=np.arange(field.x.array.size)
+    field.x.array[0:field.function_space.dofmap.index_map.size_local]= np.arange(*field.function_space.dofmap.index_map.local_range)
+    field.x.scatter_forward()
