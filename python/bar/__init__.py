d2edd3acd8314f0dc028525a1046bcf9a15f25df
Salzman Alexis <alexis.salzman@ec-nantes.fr>
2026-07-31 12:21:01 +0200

    introduce switch version mecanism

--- python/bar/__init__.py
+++ python/bar/__init__.py
@@ -1,4 +1,9 @@
 from bar.util import setf
+try:
+    from ._version import __version__
+except ImportError:
+    __version__ = "0+unknown"
+
 from bar.bar_cpp import foo
 __all__ = [
         "foo",
