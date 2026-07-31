ebbd1c6249093c08ba486dd3ab59651c5857afdb
Salzman Alexis <alexis.salzman@ec-nantes.fr>
2026-07-31 12:52:08 +0200

    for sckit<1

--- python/bar/__init__.py
+++ python/bar/__init__.py
@@ -1,6 +1,13 @@
 from bar.util import setf
+
+# to replace by simple version if scikit>1.0
+from importlib.metadata import version, PackageNotFoundError
+
 try:
-    from ._version import __version__
+    __version__ = version("bar")
+#replacing version with  scikit>1.0
+#try:
+#    from ._version import __version__
 except ImportError:
     __version__ = "0+unknown"
 
