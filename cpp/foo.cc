d43e19b6b3f1a8785daf1a6b1bde610c42ad2765
Salzman Alexis <alexis.salzman@ec-nantes.fr>
2026-07-31 15:08:16 +0200

    add a compilation bugg

--- cpp/foo.cc
+++ cpp/foo.cc
@@ -5,5 +5,5 @@ double foo(const std::shared_ptr<dolfinx::fem::Function<double>> field)
    auto r = field->x()->array();
    auto size_local=field->function_space()->dofmap()->index_map->size_local();
 
-   return std::accumulate(r.begin(), r.begin()+size_local, 30.);
+   return std::accumulate(r.begin(), r.begin()+size_local, Z30.);
 }
